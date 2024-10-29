358ec70d2dcf978657563bc85993b6609a01d137
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-10-29 10:26:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes.forEach((code, idx) => {
+  code.addEventListener('input', () => {
+    if (code.value.length === 1 && idx < codes.length - 1) {
+      codes[idx + 1].focus();
+    }
+  });
+
+  code.addEventListener('keydown', (event) => {
+    if (event.key === 'Backspace' && code.value === '' && idx > 0) {
+      codes[idx - 1].focus();
+    }
+  });
+});
